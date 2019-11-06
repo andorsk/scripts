bb12fbf10d1bce496f700b295c1cc896e7c7349c
and0rsk <andor@hensisknot.com>
2019-11-06 16:54:21 +0530

    fixed script

--- TerminalImageViewer.sh
+++ TerminalImageViewer.sh
@@ -1,8 +1,21 @@
 #/!bin/bash
 PARENTPATH=$(dirname "${BASH_SOURCE[0]}")
 if [[ ( ! -z "$1") && ($1 = "help" ) ]]; then 
-    echo "Run image in terminal"
+    echo "Run image in terminal. Usage: TerminalImageViewer.sh <fontsize> <img_path>. If no fontzie, then uses default font size"
     exit
 fi
+VERSION=$1
+echo -e "\033]50;SetProfile=FontSize${VERSION}\a" 
 
-timg "$@"
+re='^[0-9]+$'
+
+if ! [[ $VERSION =~ $re ]] ; then
+    timg "${@}"
+else
+    timg "${@:2}"
+fi
+
+
+read -e finish
+
+echo -e "\033]50;SetProfile=Default\a"
